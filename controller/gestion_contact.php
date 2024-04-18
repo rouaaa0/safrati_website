d842b0627a632c5e53c5e4182aa106c467998653
roua abderrahim <roua.abderrahim@esprit.tn>
2024-04-18 18:57:15 +0100

    CRUD

---
+++ controller/gestion_contact.php
@@ -0,0 +1,139 @@
+<?php
+
+include_once("C:/xampp/htdocs/mcv/config.php");
+include_once("C:/xampp/htdocs/mcv/Model/contact.php");
+
+
+
+
+class contact_gestion
+{
+   
+
+    function addcontact($contact)
+{
+    $sql = "INSERT INTO contact  VALUES (NULL,:sujet_contact,:date_envoie,DEFAULT,:description)";
+
+    $config = new Config();
+    $db = $config->getConexion();   
+    try {
+        $query = $db->prepare($sql);
+        $query->execute([
+            'sujet_contact' => $contact->getSujet_contact(),
+            'date_envoie' => $contact->getDateEnvoie()->format('Y-m-d H:i:s'),
+            'description' => $contact->getdescription(),
+        ]);
+        // Gérer le succès de l'insertion ici si nécessaire
+    } catch (PDOException $e) {
+        // Gérer les erreurs de la base de données ici
+        echo 'Error: ' . $e->getMessage();
+    }
+}
+public function listContact()
+{
+    $sql = "SELECT * FROM contact";
+    $config = new Config();
+    $db = $config->getConexion();
+    try {
+        $liste = $db->query($sql);
+        return $liste;
+    } catch (Exception $e) {
+        die('Error:' . $e->getMessage());
+    }
+}
+function deleteContact($id)
+{
+    $sql = "DELETE FROM contact WHERE id_contact = :id";
+
+    $config = new Config();
+    $db = $config->getConexion();
+    $req = $db->prepare($sql);
+    $req->bindValue(':id', $id);
+
+    try {
+        $req->execute();
+    } catch (Exception $e) {
+        die('Error:' . $e->getMessage());
+    }
+}
+function showContact($id)
+{
+    $sql = "SELECT * from contact where id_contact = $id";
+    $config = new Config();
+    $db = $config->getConexion();
+    try {
+        $query = $db->prepare($sql);
+        $query->execute();
+
+        $contact = $query->fetch();
+        return $contact;
+    } catch (Exception $e) {
+        die('Error: ' . $e->getMessage());
+    }
+}
+
+
+
+function updateContact($contact, $id)
+{
+    try {
+        $config = new Config();
+        $db = $config->getConexion();
+        
+        $query = $db->prepare('UPDATE contact SET sujet_contact = :sujet, date_envoie = :date_envoie, etat_contact = :etat, description = :description WHERE id_contact = :id');
+        $query->execute([
+            'id' => $id,
+            'sujet' => $contact->getSujet_contact(),
+            'date_envoie' => $contact->getDateEnvoie()->format('Y-m-d H:i:s'),
+            'etat' => $contact->getEtat_contact(),
+            'description' => $contact->getdescription()
+        ]);
+        // echo $query->rowCount() . " records UPDATED successfully <br>";
+    } catch (PDOException $e) {
+        $e->getMessage();
+    }
+}
+
+
+
+
+
+}  
+
+
+
+
+class reponse_gestion
+{
+   
+    function addReponse($reponse)
+    {  
+     $sql = "INSERT INTO reponse  VALUES (NULL,:idContact,:Reponse,:date_envoie_r,DEFAULT)";
+
+            $config = new Config();
+            $db = $config->getConexion();   
+         try {
+            //print_r($reponse->getDateEnvoie_r()->format('Y-m-d H:i:s'));
+            $query = $db->prepare($sql);
+            $query->execute([
+                'idContact' => $reponse->getididContact(),
+                'Reponse' => $reponse->getReponse(),
+                'date_envoie_r' => $reponse->getDateEnvoie_r()->format('Y-m-d H:i:s'),
+
+            ]);
+        } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+    }
+
+   
+
+
+
+  
+}
+
+
+
+
+?>
---
+++ controller/traitement_contact.php
@@ -0,0 +1,34 @@
+<?php
+include 'C:/xampp/htdocs/mcv/controller/gestion_contact.php';
+
+
+
+$error = "";
+
+// create reclamation
+$contact = null;
+
+$curentDate= date('Y-m-d H:i:s');
+
+//controsaisi mtaa reclamation
+// create an instance of the controller
+$contact_gestion = new contact_gestion();
+if (isset($_POST["sujet"]) && isset($_POST["message"])) 
+{
+    if (  (!empty($_POST["sujet"]) && !empty($_POST["message"])) )
+     {
+        $contact = new contact(null,new DateTime($curentDate) ,$_POST['sujet'] , $_POST['message'] ); 
+        $contact_gestion->addcontact($contact);
+        
+
+
+        
+
+    } else
+        {$error = "Missing information";
+         echo $error ; }
+
+}
+
+
+?>
